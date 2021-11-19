4feb606d122d27a00df3808885ad9c948d0b743f
Albena Kostova <albena.kostov@gmail.com>
2021-11-19 09:54:44 +0100

    .

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+import './Navbar.css';
+import Temple from '../assets/temple.svg';
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <ul>
+                <li className="logo">
+                    <img src={Temple} alt="dojo logo" />
+                    <span>Dojo</span>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">Signup</Link>
+                </li>
+                <li>
+                    <button className="btn">Logout</button>
+                </li>
+            </ul>
+        </div>
+    )
+}
