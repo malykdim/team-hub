7016cbfbdcf4a494cb9b44db9290aabc6d1614f8
Albena Kostova <albena.kostov@gmail.com>
2021-11-20 14:19:36 +0100

    fetching users & online

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import OnlineUsers from './components/OnlineUsers';
 
 function App() {
   const { user, authIsReady } = useAuthContext();
@@ -45,6 +46,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
