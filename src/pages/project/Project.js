acc2fdac31886974c96260eed10ad9ae7f87afe2
Albena Kostova <albena.kostov@gmail.com>
2021-11-21 11:29:39 +0100

    project summary

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,9 +1,24 @@
+import { useParams } from 'react-router';
+
+import { useDocument } from '../../hooks/useDocument';
+import ProjectSummary from './ProjectSummary';
 import './Project.css';
 
 export default function Project() {
+    const { id } = useParams();
+    const { error, document } = useDocument('projects', id);
+    
+    if (error) {
+        return <div className="error">{error}</div>
+    }
+    
+    if (!document) {
+        return <div className="loading">Loading...</div>
+    }
+    
     return (
-        <div>
-            Project
+        <div className="project-details">
+            <ProjectSummary project={document} />
         </div>
-    )
+    );
 }
