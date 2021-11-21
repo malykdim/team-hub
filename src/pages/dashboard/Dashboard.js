01e34c5535fa1836c0e7fa4fad6457e8406a2ad5
Albena Kostova <albena.kostov@gmail.com>
2021-11-21 10:25:21 +0100

    projects list / dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,17 @@
+import { projectAuth } from '../../firebase/config';
+import { useCollection } from '../../hooks/useCollection';
+
 import './Dashboard.css';
+import ProjectList from '../../components/ProjectList';
 
 export default function Dashboard() {
+    const { documents, error } = useCollection('projects');
+    
     return (
         <div>
-            Dashboard
+            <h2 className="page-title">Dashboard</h2>
+            {error && <p className="error">{error}</p>}
+            {documents && <ProjectList projects={documents} />}
         </div>
     )
 }
